518efad2a8c2579ffd1b32c3569889dabde962e2
Michael Collier <mikec@kuali.co>
2017-11-24 14:58:48 -0700

    C R E A T I O N

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+    <h2>This is it baby</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "This is the message!"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #42b983;
+}
+</style>
