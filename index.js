f0546de65d2db9b54e49884be67d4f88e712b1bf
Michael <mikec@kuali.co>
2017-12-12 23:11:37 -0700

    Include dist

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express")
 
 const app = express()
 
-app.use(express.static("dist"))
+app.use(express.static(process.cwd() + "/dist/"))
 
 app.listen(process.env.PORT || 3000)
